7662d101e8a18a599f809f7da5e2ea68158e7e32
RJzz <jieruiruir@gmail.com>
2016-12-04 20:35:19 +0800

    完成数据库插入

--- src/model/Duty.java
+++ src/model/Duty.java
@@ -4,4 +4,47 @@ package model;
  * Created by RJzz on 2016/12/3.
  */
 public class Duty {
+    private String dDid;
+    private String dName;
+    private String dEmail;
+    private String dPhone;
+    public Duty(String dDid, String dName, String dEmail, String dPhone) {
+        this.dDid = dDid;
+        this.dName = dName;
+        this.dEmail = dEmail;
+        this.dPhone = dPhone;
+    }
+
+    public void setdDid(String dDid) {
+        this.dDid = dDid;
+    }
+
+    public void setdEmail(String dEmail) {
+        this.dEmail = dEmail;
+    }
+
+    public void setdName(String dName) {
+        this.dName = dName;
+    }
+
+    public void setdPhone(String dPhone) {
+        this.dPhone = dPhone;
+    }
+
+    public String getdDid() {
+        return dDid;
+    }
+
+    public String getdEmail() {
+        return dEmail;
+    }
+
+    public String getdName() {
+        return dName;
+    }
+
+    public String getdPhone() {
+        return dPhone;
+    }
+
 }
--- src/model/Keeper.java
+++ src/model/Keeper.java
@@ -4,5 +4,47 @@ package model;
  * Created by RJzz on 2016/12/3.
  */
 public class Keeper {
+    private String kKId;
+    private String kName;
+    private String KEmail;
+    private String kPhone;
+    public Keeper(String id, String name, String email, String phone) {
+        this.kKId = id;
+        this.kName = name;
+        this.KEmail = email;
+        this.kPhone = phone;
+    }
 
+    public String getKEmail() {
+        return KEmail;
+    }
+
+    public String getkKId() {
+        return kKId;
+    }
+
+    public String getkName() {
+        return kName;
+    }
+
+    public String getkPhone() {
+        return kPhone;
+    }
+
+    public void setKEmail(String KEmail) {
+        this.KEmail = KEmail;
+    }
+
+    public void setkKId(String kKId) {
+        this.kKId = kKId;
+    }
+
+    public void setkName(String kName) {
+        this.kName = kName;
+    }
+
+    public void setkPhone(String kPhone) {
+        this.kPhone = kPhone;
+    }
 }
+
